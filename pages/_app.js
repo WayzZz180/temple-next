e0011782aba5739251a93449c0ff4e7186f20bb1
Hannah <chize9863@gmail.com>
2023-07-03 19:16:52 +0800

    Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 // import '@/styles/globals.css'
 // import '@/styles/book-list.css'
-
+import Layout from '@/components/layout/default-layout'
 import { AuthProvider } from '@/hooks/use-auth'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => <Layout>page</Layout>)
 
   return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
 }
--- pages/project/footer.js
+++ pages/project/footer.js
@@ -1,5 +1,3 @@
-import Footer from '@/components/layout/footer'
-
 export default function PartFooter() {
-  return <Footer />
+  return <></>
 }
