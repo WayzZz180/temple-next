47e08a624f029e754e1284f71456945e14e0c6ac
Hannah <chize9863@gmail.com>
2023-07-04 10:16:17 +0800

    del imgs & add blank layout

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { AuthProvider } from '@/hooks/use-auth'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => <Layout>page</Layout>)
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
 
   return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
 }
