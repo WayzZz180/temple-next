27437ee477e667ff13ece267360f403cf189d4dd
Hannah <chize9863@gmail.com>
2023-07-04 10:21:23 +0800

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-// import '@/styles/globals.css'
-// import '@/styles/book-list.css'
-
+import '@/styles/globals.css'
 import { AuthProvider } from '@/hooks/use-auth'
+import Layout from '@/components/layout/default-layout'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
 
   return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
 }
