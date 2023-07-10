d8a5d69626a25c45221d7504637d3555c076539e
leodder <a0919989772@gmail.com>
2023-07-10 13:09:45 +0800

    header_final

--- components/layout/header/index.js
+++ components/layout/header/index.js
@@ -3,24 +3,24 @@ import styles from './header.module.sass'
 import NavbarLeft from './navbarleft'
 import NavbarRight from './navbarright'
 import Logo from './logo'
-// import Image from 'next/image'
-// import Header_Tri from '@/assets/header_tri.svg'
 
 export default function Index() {
   return (
     <>
-      <header>
-        {/* navbar left */}
-        <div className={`${styles.flex_row2} ${styles.h15px} ${styles.head}`}>
-          <NavbarLeft />
+      <header className={`${styles.header_container}`}>
+        <div>
+          {/* navbar left */}
+          <div className={`${styles.flex_row2} ${styles.h15px} ${styles.head}`}>
+            <NavbarLeft />
 
-          {/* logo */}
-          <Logo />
+            {/* logo */}
+            <Logo />
 
-          {/* navbar right*/}
-          <NavbarRight />
+            {/* navbar right*/}
+            <NavbarRight />
+          </div>
+          {/* <Image src={Header_Tri} alt="tri" /> */}
         </div>
-        {/* <Image src={Header_Tri} alt="tri" /> */}
       </header>
     </>
   )
