f51ca789101ac777f456418973775c7b8a8e6526
hchhannah <chize9863@gmail.com>
2023-07-10 10:47:15 +0800

    button bgColor + title modify

--- components/common/button/index.js
+++ components/common/button/index.js
@@ -1,10 +1,19 @@
 import styles from './button.module.sass'
+import variables from '@/styles/_variables.module.sass'
 
+// text = Button content
+// btnColor = black, green, hot_pink, brown
 export default function Button({ text = 'text', btnColor = 'btn' }) {
+  const var_color = variables[btnColor]
   return (
     <>
       <div>
-        <button className={`${styles.btn} ${styles[btnColor]}`}>{text}</button>
+        <button
+          className={`${styles.btn}`}
+          style={{ backgroundColor: var_color }}
+        >
+          {text}
+        </button>
       </div>
     </>
   )
