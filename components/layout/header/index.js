229cbea4e98cafe7f9062bb7227f9fa1b7b94a76
hchhannah <chize9863@gmail.com>
2023-07-14 12:02:53 +0800

    header reset

--- components/layout/header/index.js
+++ components/layout/header/index.js
@@ -6,22 +6,18 @@ import Logo from './logo'
 import headerBg from '@/assets/header.svg'
 
 export default function Header() {
-  const headerStyle = {
-    backgroundImage: `url(${headerBg.src})`,
-    // backgroundAttachment: "fixed",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "-1px -1px",
-    backgroundSize: "101%"
-
-  };
-
   return (
-    <header style={headerStyle}>
+    <header>
       <div className={`${styles.flex_row} ${styles.head} pt20px`}>
         <Navbar info="left" />
         <Logo />
         <Navbar info="right" />
       </div>
+      <Image
+        src={headerBg}
+        alt="header"
+        className={`${styles.headerBg}`}
+      ></Image>
       <div className={`${styles.box}`}></div>
     </header>
   )
