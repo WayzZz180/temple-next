bc4df5edda93ccf2d8b1361d29217ad7a1512177
leodder <a0919989772@gmail.com>
2023-07-18 17:32:38 +0800

    0718-3

--- pages/forum/forumgossip.js
+++ pages/forum/forumgossip.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import styles from './forumgossip.module.sass'
 import Title from '@/components/common/title/'
+// import variables from '@/styles/_variables.module.sass'
+import Forumline from '@/components/common/forumlogo/forumline'
 
 export default function Forumgossip() {
   return (
     <>
-      <div className={`${styles.flex}`}>
-        <Title text="熱門文章" text2="5678" />
-        <hr></hr>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.flex}`}>
+          <Title text="熱門文章" text2="5678" />{' '}
+        </div>
+        <Forumline lineColor="brown" />
       </div>
     </>
   )
