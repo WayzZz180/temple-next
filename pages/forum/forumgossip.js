3e608823bbaf378d78527a5d98f20a6599afb155
leodder <a0919989772@gmail.com>
2023-07-20 15:44:51 +0800

    0720

--- pages/forum/forumgossip.js
+++ pages/forum/forumgossip.js
@@ -3,6 +3,7 @@ import styles from './forumgossip.module.sass'
 import Title from '@/components/common/title/'
 // import variables from '@/styles/_variables.module.sass'
 import Forumline from '@/components/common/forumlogo/forumline'
+import Navbar from '@/components/common/forumlogo/navbar'
 
 export default function Forumgossip() {
   return (
@@ -11,6 +12,7 @@ export default function Forumgossip() {
         <div className={`${styles.flex}`}>
           <Title text="熱門文章" text2="5678" />{' '}
         </div>
+        <Navbar />
         <Forumline lineColor="brown" />
       </div>
     </>
