a35c919b9f49e44610170a61d4f13080dfb819c0
Wayz <wayz180@gmail.com>
2023-07-21 16:58:03 +0800

    button

--- components/common/button/index.js
+++ components/common/button/index.js
@@ -11,11 +11,13 @@ export default function Button({
   width = '',
   padding = '15px 60px',
   fontSize = '24px',
+  link = '',
 }) {
   const var_color = variables[btnColor]
   return (
     <div>
       <button
+        onClick={link}
         className={`${styles.btn}`}
         style={{
           backgroundColor: var_color,
