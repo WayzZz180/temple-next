a9cd348eaca9d82b259acea8f7278b4fcdebc251
leodder <a0919989772@gmail.com>
2023-07-21 16:48:40 +0800

    0721-2

--- components/common/forumlogo/forumper.js
+++ components/common/forumlogo/forumper.js
@@ -15,35 +15,42 @@ export default function Forumper() {
     <>
       <div>
         <div>
-          <div className={`${styles.flex_row1}`}>
-            <li>吸貓是快樂泉源</li>
-            <li>·</li>
-            <li>2023-05-31 19:30</li>
-          </div>
           <div>
-            <div className={`${styles.flex_col2}`}>
-              <div className={`${styles.title}`}>
-                #討論 玩遊戲就是要配XX才爽！😝打完吃什麼宵夜好？🤤
+            <div className={`${styles.flex_row1}`}>
+              <li>吸貓是快樂泉源</li>
+              <li>·</li>
+              <li>2023-05-31 19:30</li>
+            </div>
+            <div className={`${styles.flex}`}>
+              <div className={`${styles.flex_row}`}>
+                <div className={`${styles.flex_col2}`}>
+                  <div className={`${styles.title}`}>
+                    #討論 玩遊戲就是要配XX才爽！😝打完吃什麼宵夜好？🤤
+                  </div>
+                  <div className={`${styles.content}`}>
+                    「打完要不要吃宵夜？」 「怎麼一下就沒了？」
+                    玩遊戲的時候總是希望旁邊有袋洋芋片、配...
+                  </div>
+                </div>
+                <div className={`${styles.flex_row3}`}>
+                  <div className={`${styles.setmid}`}>
+                    <FavoriteBorderSharpIcon className={`${styles.marr}`} />
+                    999
+                  </div>
+                  <div className={`${styles.setmid}`}>
+                    <ChatBubbleOutlineSharpIcon className={`${styles.marr}`} />
+                    999
+                  </div>
+                  <div className={`${styles.setmid}`}>
+                    <BookmarkBorderSharpIcon />
+                  </div>
+                </div>
               </div>
-              <div className={`${styles.content}`}>
-                「打完要不要吃宵夜？」 「怎麼一下就沒了？」
-                玩遊戲的時候總是希望旁邊有袋洋芋片、配...
-              </div>
+              <FigureExample />
             </div>
           </div>
-          <div className={`${styles.flex_row3}`}>
-            <div className={`${styles.setmid}`}>
-              <FavoriteBorderSharpIcon className={`${styles.marr}`} />
-              999
-            </div>
-            <div className={`${styles.setmid}`}>
-              <ChatBubbleOutlineSharpIcon className={`${styles.marr}`} />
-              999
-            </div>
-            <div className={`${styles.setmid}`}>
-              <BookmarkBorderSharpIcon />
-            </div>
-          </div>
+
+          {/* <div className={`${styles.flex_end}`}>123</div> */}
         </div>
         <Forumline lineColor="brown" />
       </div>
