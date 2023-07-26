9cf7d3e6288fb73501246119a50725c794b090ed
Hannah <chize9863@gmail.com>
2023-07-26 21:58:42 +0800

    order animation

--- pages/shop/order/index.js
+++ pages/shop/order/index.js
@@ -1,4 +1,5 @@
 import styles from './order.module.sass'
+import Image from 'next/image'
 
 // hooks
 import { useState, useEffect } from 'react'
@@ -9,11 +10,16 @@ import { useClick } from '@/hooks/useClick.js'
 import ShopStepBar from "@/components/common/bar/ShopStepBar"
 import InputBox from '@/components/common/inputBox'
 import Button from '@/components/common/button'
-import ShopCartContentCard from '@/components/common/cards/ShopCartContentCard'
+import ShopOrderContentCard from '@/components/common/cards/ShopOrderContentCard'
+import Title from '@/components/common/title'
+
 // bootstrap
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+
+//svg
+import Arrow from '@/assets/orderArrow.svg'
 
 export default function Order() {
     const [data, setData] = useState([])
@@ -36,6 +42,10 @@ export default function Order() {
             setData(data)
             })
     }, [router.query])
+      // 小計
+  const total = data?.reduce((result, v) => {
+    return result + v.product_price * v.quantity;
+  }, 0);
 
   return (
     <Container className={`${styles.container}`}> 
@@ -47,29 +57,40 @@ export default function Order() {
                 <div className={`${styles.totalBox} pt30px pb30px`}
                 onClick={handleOpenClick}
                 >
-                    <div className={` fs28px fwBolder`}>合計：NT$150</div>
-                    <div className={`fs24px`}>購物車：3件</div>
+                    <div className={` fs28px fwBolder`}>合計：NT${total}</div>
+                    <div className={`${styles.flex_row} fs24px`}>
+                        <div>
+                            購物車：{data?.length}件&nbsp;
+                        </div>
+                        <div className={`${openClickState ? styles.arrowDown : styles.arrowUp } pt5px`}
+                        > 
+                            <Image src={Arrow} alt="open" width={20}/>
+                        </div>
+                    </div>
                 </div>
             </Col>
         </Row>
         {/* 商品內容 */}
-        <Row className={`${styles.trans}`} style={{display: openClickState ? '' : 'none'}}>
+        <Row
+        // style={{display: openClickState ? '' : 'none'}}
+        >   <div  className={`${openClickState ? styles.open : styles.close}
+        `} >
+        {/* ${openClickState ? '' : styles.hidden} */}
             {data?.map((v,i) => (
-                <ShopCartContentCard
+                <ShopOrderContentCard
                 key={i}
                 src={`/${v.image}`}
                 name={`${v.product_name}`}
                 price={`${v.product_price}`}
-                quantity={`${v.quantity}`}
-                stock_num={`${v.stock_num}`}
-                pid={`${v.pid}`}
-                cid={`${v.cid}`}      
+                quantity={`${v.quantity}`}  
                 />
             ))
           }
+          </div>
         </Row>
         {/* 表單 */}
-        <Container className='mt100px'>
+        <Container className='mt50px'>
+            <Title  text='訂單資訊' text2='information'/>
             <Row className={`${styles.flex_space_between}`}>
                 <InputBox type="text" prompt="收件人姓名" placeholder="沈子威" value={""} onChange={()=>{}} width={600} height={60}/>
                 <InputBox type="text"  prompt="物流方式" placeholder="宅配" onChange width={600} height={60}/>
@@ -92,7 +113,9 @@ export default function Order() {
                 height = ''
                 padding = '15px 60px'
                 fontSize = '24px'
-                link = {()=>{}}
+                link = {()=>{
+                    router.push('/shop/cart')
+                }}
                 />
                 <Button text="送出訂單" 
                 btnColor = 'hot_pink'
