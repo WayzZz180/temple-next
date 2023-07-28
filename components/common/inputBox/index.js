a299746374c2ed14b660ebf977cee122fface948
Wayz <wayz180@gmail.com>
2023-07-28 18:25:15 +0800

    inputbox

--- components/common/inputBox/index.js
+++ components/common/inputBox/index.js
@@ -10,14 +10,6 @@ export default function InputBox({
   width = '453px',
   value = '',
   height = '45px',
-<<<<<<< HEAD
-  // 49 -2*2 border = 45
-  name = '',
-}) {
-  // const [inputValue, setInputValue] = useState('')
-  const [isFocus, setIsFocus] = useState(false)
-=======
->>>>>>> origin/Wayz
 
   isError,
   showError, // 新增一個 showError 屬性來控制錯誤訊息的顯示
@@ -34,15 +26,9 @@ export default function InputBox({
   }
 
   return (
-<<<<<<< HEAD
-    <div>
-      <div className="mb10px fs20px"> {prompt}</div>
-      <div>
-=======
     <div className={styles.input_box_container}>
       <div className={styles.prompt}>{prompt}</div>
       <div className={styles.input_box_wrapper}>
->>>>>>> origin/Wayz
         <input
           type={type}
           value={value}
@@ -52,15 +38,10 @@ export default function InputBox({
           onBlur={handleBlur}
           placeholder={placeholder}
           style={{ width, height }}
-<<<<<<< HEAD
-          name={name}
-          className={`${isFocus ? styles.focus : ''} ${styles.standard_input}`}
-=======
           // Apply error styles based on the isError prop
           className={`${styles.standard_input} ${
             isError ? styles.error_input : ''
           } ${isFocus ? styles.standard_focus : ''}`}
->>>>>>> origin/Wayz
         />
         {/* 根據 isError 屬性來決定是否顯示錯誤訊息 */}
         {isError && <div className={styles.error_message}>{errorMessage}</div>}
