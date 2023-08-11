aca74f51a27c75f21f8bf951b4b4b69e7e21d3fd
Hannah <chize9863@gmail.com>
2023-08-11 18:32:03 +0800

    login

--- pages/member/login.js
+++ pages/member/login.js
@@ -67,134 +67,146 @@ export default function Login() {
   }
 
   return (
-    <div className={styles.flex}>
-      <Container>
-        <Row>
-          <Col>
-            <div className="mt100px">
-              <Image
-                src={doorGodLeft}
-                alt="doorGodLeft"
-                height={835}
-                width={631}
-              ></Image>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <Container className={styles.flex}>
+      <Row>
+        <Col>
+          <div className="mt100px">
+            <Image
+              src={doorGodLeft}
+              alt="doorGodLeft"
+              // height={835}
+              width={525}
+            ></Image>
+          </div>
+        </Col>
+      </Row>
 
-      <Container>
-        <form onSubmit={doLogin}>
-          <Row>
-            <Col>
-              <MemberTitle text="登入會員" text2="LOGIN" lineColor="green" />
-            </Col>
-          </Row>
-          <Row className={styles.flex_centre}>
-            <Col>
-              <InputBox
-                prompt="測試用~Email"
-                type="email"
-                // className="form-control"
-                id="member_account"
-                placeholder="電子郵件地址"
-                value={user.member_account}
-                onChange={changeUser}
-              />
-            </Col>
-          </Row>
-          <Row className={styles.flex_centre}>
-            <Col>
+      <Row className="ps60px pe60px">
+        <Col>
+          <div className="mt100px">
+            <MemberTitle
+              text="登入會員"
+              text2="LOGIN"
+              lineColor="green"
+              width={450}
+            />
+          </div>
+        </Col>
+        <Col className={styles.flex_centre}>
+          <div>
+            <InputBox
+              prompt="帳號"
+              type="email"
+              // className="form-control"
+              id="member_account"
+              placeholder="電子郵件地址"
+              value={user.member_account}
+              onChange={changeUser}
+            />
+          </div>
+        </Col>
+        <Col className={styles.flex_centre}>
+          <div>
+            <div className="">
               <InputBox
                 type={showPassword ? 'text' : 'password'}
+                prompt="密碼"
                 // className="form-control"
                 id="member_password"
                 placeholder="密碼"
                 value={user.member_password}
                 onChange={changeUser}
               />
-              {showPassword ? (
-                <div className={styles.flex_start}>
-                  <VisibilityOffIcon
-                    onClick={toggleShowPassword}
-                    className="me10px"
-                  />
-                  隱藏密碼{' '}
-                </div>
-              ) : (
-                <div className={styles.flex_start}>
-                  <VisibilityIcon
-                    onClick={toggleShowPassword}
-                    className="me10px"
-                  />
-                  顯示密碼
-                </div>
-              )}
-            </Col>
-          </Row>
-          <Row>
-            <Col className={styles.flex_checkbox}>
-              <Checkbox label="保持登入狀態?" />
+            </div>
+            {showPassword ? (
+              <div className={styles.flex_start} style={{ cursor: 'pointer' }}>
+                <VisibilityOffIcon
+                  onClick={toggleShowPassword}
+                  className="me10px"
+                />
+                隱藏密碼{' '}
+              </div>
+            ) : (
+              <div className={styles.flex_start} style={{ cursor: 'pointer' }}>
+                <VisibilityIcon
+                  onClick={toggleShowPassword}
+                  className="me10px"
+                />
+                顯示密碼
+              </div>
+            )}
+          </div>
+        </Col>
+        <Col>
+          <div className={styles.flex_checkbox}>
+            <Checkbox label="保持登入狀態？" />
+            <Link href="#" className={`${styles.linkHover} link fwBold`}>
+              忘記密碼？
+            </Link>
+          </div>
+        </Col>
+
+        <Col className={`${styles.flex_container} fwBold`}>
+          <div>
+            <div className="ls6px">
+              如登入，即同意錦囊廟祭的
               <Link href="#" className="link">
-                忘記密碼？
+                隱私權政策
               </Link>
-            </Col>
-          </Row>
+              和
+              <Link href="" className="link">
+                使用條款
+              </Link>
+            </div>
+          </div>
+        </Col>
+        <Col className={styles.flex_centre}>
+          <div className="mb15px">
+            <Button
+              text="登入"
+              btnColor="black"
+              width={487}
+              link={() => {
+                doLogin()
+              }}
+            />
+          </div>
+        </Col>
 
-          <Row className={styles.flex_container}>
-            <Col>
-              <div className="ls6px">
-                如登入，即同意錦囊廟祭的
-                <Link href="#" className="link">
-                  隱私權政策
-                </Link>
-                和
-                <Link href="" className="link">
-                  使用條款
-                </Link>
-              </div>
-            </Col>
-          </Row>
-          <Row className={styles.flex_centre}>
-            <Col>
-              <Button
-                text="登入"
-                btnColor="black"
-                width={487}
-                link={() => {
-                  doLogin()
-                }}
-              />
-            </Col>
-          </Row>
+        <Col className={styles.flex_centre}>
+          <div>
+            <div className={`${styles.join}`}>
+              {/* 不是會員？
+              <Link href="/member/signUp" className="link">
+                加入我們
+              </Link> */}
+              <div className="fs18px fwBold pe5px">不是會員？</div>
 
-          <Row className={styles.flex_centre}>
-            <Col>
-              <div>
-                不是會員?{' '}
-                <Link href="/member/signUp" className="link">
-                  加入我們
-                </Link>
-              </div>
-            </Col>
-          </Row>
-        </form>
-      </Container>
+              <Link
+                href="/member/signUp"
+                className={`${styles.linkHover} link fs18px fwBold`}
+              >
+                加入我們
+              </Link>
+            </div>
+          </div>
+        </Col>
+      </Row>
 
-      <Container>
-        <Row>
-          <Col>
+      <Row>
+        <Col>
+          <div>
             <div className="mt100px">
               <Image
                 src={doorGodRight}
                 alt="doorGodRight"
-                height={835}
-                width={631}
+                // height={835}
+                width={525}
               ></Image>
             </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
