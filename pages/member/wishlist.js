db08db7e31661d1935fcce5585fb354cc5f5bb47
hchhannah <chize9863@gmail.com>
2023-08-11 13:16:20 +0800

    wishlist

--- pages/member/wishlist.js
+++ pages/member/wishlist.js
@@ -32,17 +32,18 @@ export default function MyWishlist() {
       })
         .then((r) => r.json())
         .then((data) => {
-          console.log(`wishlist頁面 有沒有auth.token?2`, auth.token)
-          console.log(`wishlist頁面 data:`, data)
+          // console.log(`wishlist頁面 有沒有auth.token?2`, auth.token)
+          // console.log(`wishlist頁面 data:`, data)
           // 進入頁面把資料抓出來
           setWishlist(data)
         })
-    } else {
-      // Handle the case when auth.token is not available or user is not logged in
-      // You can add any additional logic here
-      console.log('用戶尚未註冊')
     }
-  }, [router.query, reset])
+    // else {
+    // Handle the case when auth.token is not available or user is not logged in
+    // You can add any additional logic here
+    // console.log('用戶尚未註冊')
+    // }
+  }, [reset])
 
   return (
     <div className={styles.flex}>
