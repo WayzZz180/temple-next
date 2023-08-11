81522380e2acd2327b532eb9f1d997e973609122
leodder <a0919989772@gmail.com>
2023-08-11 16:34:47 +0800

    comment

--- pages/forum/[category]/[post_sid]/index.js
+++ pages/forum/[category]/[post_sid]/index.js
@@ -5,9 +5,12 @@ import Forumline from '@/components/common/forumlogo/forumline'
 import Avatar from '@mui/material/Avatar'
 import Image from 'next/image'
 import Head from 'next/head'
+import TextArea from '@/components/common/inputBox/textarea'
+import ForumButton from '@/components/common/button/'
 
 export default function ReadPost({ src = '', post }) {
   const router = useRouter()
+  const [inputComment, setInputComment] = useState('')
   //   console.log(router)
   // const [row, setRow] = useState(post)
   const [data, setData] = useState([])
@@ -69,6 +72,7 @@ export default function ReadPost({ src = '', post }) {
         </div>
         <Forumline />
         <div className={`${styles.col}`}>
+          {/* 讀出留言 */}
           <div className={`${styles.row4}`}>
             <div>
               <Avatar
@@ -91,6 +95,66 @@ export default function ReadPost({ src = '', post }) {
               </div>
             </div>
           </div>
+          <form>
+            <div className={`${styles.row4}`}>
+              <div>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="/static/images/avatar/1.jpg"
+                  sx={{ width: 66, height: 66 }}
+                  className={`${styles.avatar}`}
+                />
+              </div>
+              <div className={`${styles.col2}`}>
+                <div className={`${styles.commenter}`}>吸貓是快樂泉源</div>
+                <div className={`{${styles.commenttime}`}>
+                  2023-05-11-08:11:15
+                </div>
+                <div className={`${styles.comment}`}>
+                  {/* 窩魯妹啦 一大群人追著很明顯就是人為操控的轎子到處跑
+                而且對轎子是自己動的深信 窩魯妹啦
+                一大群人追著很明顯就是人為操控的轎子到處跑
+                而且對轎子是自己動的深信 */}
+                  <TextArea
+                    type="text"
+                    height="100px"
+                    width="100%"
+                    value={inputComment}
+                    onChange={
+                      (e) => {
+                        setInputComment(e.target.value)
+                      }
+                      // console.log('Input value changed:', e.target.value)
+                    }
+                    placeholder="請輸入內容"
+                    autoComplete="off"
+                    id="content"
+                    required
+                    lineheight="20px"
+                  />
+                  <div className={`${styles.button}`}>
+                    <ForumButton
+                      btnColor="green"
+                      height="10px"
+                      width="40px"
+                      fontSize="10px"
+                      text="留言"
+                      type="button"
+                      padding="20px 50px"
+                      link={(e) => {
+                        // e.preventDefault()
+
+                        addData(inputComment)
+
+                        // console.log('page:', page)
+                        // console.log('category:', router.query.category)
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </>
